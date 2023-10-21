f50697928cb1d470ef79f535494af5cc5144e745
sejin-P <chunpark37@gmail.com>
2023-10-22 00:30:41 +0900

    refactor; len

--- src/list/list.rs
+++ src/list/list.rs
@@ -8,18 +8,19 @@ impl <T> List<T> {
         List { array: Vec::with_capacity(cap as usize), cap: cap as usize}
     }
     pub fn append(&mut self, t: T) {
-        if self.array.len() == self.cap {
+        let l = self.array.len();
+        if l == self.cap {
             let new_vec = Vec::with_capacity(2*self.cap);
             let mut i = 0;
             while let Some(t) = self.array.pop() {
-                new_vec[self.array.len()-i-1] = t;
+                new_vec[l-i-1] = t;
                 i += 1;
             }
             self = &mut List { array: new_vec, cap: 2 * self.cap };
             return
         }
 
-        self.array[self.array.len()] = t;
+        self.array[l] = t;
         return
     }
 }
