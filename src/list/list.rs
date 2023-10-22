5fd34b23ef13be374bf62736e07f723a94acda48
sejin-P <chunpark37@gmail.com>
2023-10-22 17:22:15 +0900

    test;

--- src/list/list.rs
+++ src/list/list.rs
@@ -1,30 +1,49 @@
-pub struct List<T> {
+pub struct List<T: Copy> {
     array: Vec<Option<T>>,
     cap: usize,
     len: usize,
 }
 
-impl <T> List<T> {
+impl <T: Copy> List<T> {
+    // Specifically, when using the vec![value; count] syntax,
+    // the vec! macro tries to clone value count times, even if the value is something like None.
     pub fn new(cap: usize) -> Self {
         List { array: vec![None; cap], cap, len: 0 }
     }
     pub fn append(&mut self, t: T) {
-        if self.array.len() == self.cap {
+        if self.len == self.cap {
             self.double_cap();
         }
 
-        self.array[self.array.len()] = t;
+        self.array[self.len] = Some(t);
         self.len += 1;
         return
     }
 
+    pub fn get(&self, i: usize) -> Option<T> {
+        return self.array[i]
+    }
+
     fn double_cap(&mut self) {
-        let mut new_vec = vec![None, 2*self.cap];
+        let mut new_vec = vec![None; 2*self.cap];
         for i in 0..self.len {
-            new_vec[i] = self.array[i].take();
+            new_vec[i] = self.array[i].clone();
         }
 
         self.array = new_vec;
         self.cap += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::list::list::List;
+
+    #[test]
+    fn append_one() {
+        let mut li = List::new(1);
+        li.append(1);
+        let a = li.get(0).expect("");
+        assert_eq!(1, a);
     }
 }
