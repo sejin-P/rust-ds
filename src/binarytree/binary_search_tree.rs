f553866360b366c86046ebe7e6a0c8d12c8409b1
sejin-P <chunpark37@gmail.com>
2024-01-02 20:17:05 +0900

    feat; binary search tree; delete_by_merging

--- src/binarytree/binary_search_tree.rs
+++ src/binarytree/binary_search_tree.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::mem;
 use std::ops::Deref;
 use std::rc::{Rc, Weak};
 
@@ -41,6 +42,70 @@ impl<T: PartialOrd> BinarySearchTree<T> {
             Some(self)
         }
     }
+
+    pub fn delete_by_merging(&mut self, val: &T) {
+        if val < &self.val {
+            match self.left.as_mut() {
+                None => { return }
+                Some(tree) => {
+                    if &tree.val == val && tree.left.is_none() && tree.right.is_none() {
+                        self.left = None;
+                        return
+                    }
+                    return tree.delete_by_merging(val)
+                }
+            }
+        }
+
+        if val > &self.val {
+            match self.right.as_mut() {
+                None => {  }
+                Some(tree) => {
+                    if &tree.val == val && tree.left.is_none() && tree.right.is_none() {
+                        self.right = None;
+                        return
+                    }
+                    return tree.delete_by_merging(val)
+                }
+            }
+        }
+
+        if self.right.is_none() {
+            let mut left = self.left.take().unwrap();
+            self.val = left.val;
+            self.right = left.right;
+            self.left = left.left;
+
+            return
+        }
+
+        if self.left.is_none() {
+            let mut right = self.right.take().unwrap();
+            self.val = right.val;
+            self.right = right.right;
+            self.left = right.left;
+            return
+        }
+
+        let right = self.right.take().unwrap();
+        let mut left = self.left.take().unwrap();
+
+        self.val = left.val;
+        self.left = left.left;
+
+        self.attach_right(right);
+    }
+
+    fn attach_right(&mut self, right: Box<BinarySearchTree<T>>) {
+        match &mut self.right {
+            Some(right_child) => {
+                right_child.attach_right(right)
+            }
+            None => {
+                self.right = Some(right);
+            }
+        }
+    }
 }
 
 
@@ -57,5 +122,17 @@ mod tests {
         b.insert(1);
         b.insert(9);
         assert_eq!(1, b.search(&1).unwrap().val)
+    }
+
+    #[test]
+    fn delete() {
+        let mut b = BinarySearchTree::new(5);
+        b.insert(1);
+        b.insert(7);
+        b.insert(6);
+        b.insert(8);
+        b.delete_by_merging(&7);
+        assert_eq!(true, b.search(&7).is_none());
+        assert_eq!(false, b.search(&1).is_none());
     }
 }
