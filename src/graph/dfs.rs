7b13c2df7d94815b5cd475e7c26f53f3b152c2fb
sejin-P <chunpark37@gmail.com>
2024-02-04 21:48:22 +0900

    feat; dfs

---
+++ src/graph/dfs.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+pub struct Dfs<T: Copy + PartialEq> {
+    visited: HashMap<T, bool>,
+    graph: HashMap<T, Vec<T>>
+}
+
+impl<T: Copy + PartialEq> Dfs<T> {
+    pub fn new() -> Self {
+        return Dfs {
+            visited: HashMap::new(),
+            graph: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, u: T, v: T) {
+        self.graph.entry(u).or_insert_with(Vec::new).push(v);
+    }
+
+    pub fn search(&mut self, u: T) {
+        self.visited.insert(u, true);
+
+        match self.graph.get(u) {
+            None => {
+                return
+            }
+            Some(ve) => {
+                for v in ve {
+                    match self.visited.get(v) {
+                        None => {
+                            return;
+                        }
+                        Some(visited) => {
+                            if visited {
+                                return;
+                            }
+
+                            self.search(v)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
